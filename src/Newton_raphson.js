a8b2a61c563f850091454bf5e601cee199e5bc51
donut1531 <love152740@gmil.com>
2021-03-08 20:01:22 +0700

    add cal_function

--- src/Newton_raphson.js
+++ src/Newton_raphson.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {Input , Button} from 'antd';
+import {equation , fixed_fx} from '/.function';
 class Newton_raphson extends React.Component{
 
-    state = { Equation : null , X : null , E : null};
+    state = { Equation : '' , X : '' , E : '' ,status : null};
 
     getEquation = (Event) => {
         this.setState({Equation : Event.target.value})
@@ -14,7 +15,34 @@ class Newton_raphson extends React.Component{
         this.setState({E : Event.target.value})
     }
     cal_newton = (Event) =>{
+        if(Equation === '' || X === '' || E === ''){
+            this.setState({status : <div style = {{color : 'red'}}> โปรดกรอกข้อมูลให้ครบถ้วน </div>})
+            return;
+        }
+        try{
+            let Equation = this.state.Equation;
+            Equation = equation(Equation);
+            equation(2,Equation);//debug
+    
+            let X = parseFloat(this.state.X);
+            let E = parseFloat(this.state.E);
+    
+            let Error = 99999;
+            let i = 1;
+            let arr = [];
+            let x1 = 0;
+    
+            while(Error > E){
+                X0 = equation(X,Equation);
+                
+            }
+        }
+        catch(error){
+
+        }
         
+        
+
     }
 
     render(){
